1c254b499ce573245d35a4919e5b30e07af7fe66
KingDave17 <ajibadedavid1000@gmail.com>
2025-03-19 12:12:55 +0100

    new changes made

---
+++ app.js
@@ -0,0 +1,19 @@
+// const menu=document.querySelector("icon-menu img")
+// const nav=document.querySelector(".header nav")
+
+// menu.onclick=()=>{
+//     nav.style.transform="translateY(0)"
+// }
+const button=document.querySelector(".alt")
+const nav=document.querySelector(".header nav")
+
+button.onclick=()=>{
+    // nav.style.display="block"
+    if(nav.style.display == "block"){
+        nav.style.display = "none"
+        button.setAttribute("src", "./images/icon-hamburger.svg")
+    }else{
+        nav.style.display="block"
+        button.setAttribute("src", "images/icon-close-menu.svg")
+    }
+}
